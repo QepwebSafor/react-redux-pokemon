6a94aa314ebc6c35d44d875379c2ff9477c05340
QepwebSafor <qepsaforweb@gmail.com>
2022-07-05 23:32:01 +0200

    Mi primer commit

--- src/index2.js
+++ src/index2.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {  applyMiddleware, compose } from 'redux';
+import { applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import {configureStore} from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
 import App from './containers/App';
+import { pokemonReducer } from './reducers/pokemonReducer';
 import { logAction, reportError } from './middlewares';
-import rootReducer from './reducers/rootReducer';
 import './index.css';
+const composeAlt = window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
+const composedEnhancers = composeAlt(
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const composedEnhancers = composeEnhancers(
   applyMiddleware(thunk, logAction, reportError)
 );
 
-const store = configureStore({reducer: rootReducer}, composedEnhancers);
+const store = configureStore(
+{  reducer:pokemonReducer,
+  composedEnhancers,
+})
 
 ReactDOM.render(
   <Provider store={store}>
@@ -23,3 +25,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+export default store;
